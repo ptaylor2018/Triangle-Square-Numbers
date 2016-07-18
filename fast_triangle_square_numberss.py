bb224ae9b9e8f6c62b609245001ed9d60ff542b4
Patrick Taylor <ptaylor2018@users.noreply.github.com>
2016-07-18 17:00:50 -0400

    Cleaned, added comments, added UI

--- fast_triangle_square_numberss.py
+++ fast_triangle_square_numberss.py
@@ -1,45 +1,39 @@
 '''
 Created on Jul 17, 2016
 
-@author: Patrick
+@author: Patrick Taylor (with the assistance of Kate Minker)
 '''
-#import sys
+
 from math import sqrt
-import gmpy2
-from gmpy2 import isqrt
-import time
+number_wanted= int(raw_input("How many triangle square numbers do you desire?(Can handle up to and including 200)"))
+import time # some stuff to see how long the operation takes
 start_time=time.time()
-def leap_frog(start_val, top_fact):
-    restart_val= sqrt(start_val)
-    bottom_range= long(round(restart_val*(5.82842712474619)))
-    top_range_set= long(round(restart_val*top_fact))
-    top_range= top_range_set -((top_range_set - bottom_range)/2)
-    #for i in range(bottom_range,top_range+1):
+
+def leap_frog(start_val):
+    restart_val= sqrt(start_val) # this takes the starting value in the list of triangle squares and gives its square root counter part, which is used to determine bottom_range
+    bottom_range= int(round(restart_val*(5.82842712474619)))
     while(True):
-        poss_tri= bottom_range**2
-        test = poss_tri*8 +1
-        if (sqrt(test)).is_integer():
+        poss_tri= bottom_range**2 #this makes the bottom_range a square number, fulfilling criterion 1
+        test = poss_tri*8 +1 # this(with the next line) checks if poss_tri is in fact a triangle number. if test is a perfect square, it is!
+        if (sqrt(test)).is_integer(): # so I wanted to make this an integer sqrt test so I could go above 200, but isqrt is way slower than sqrt.
             return poss_tri
         else:
-            bottom_range+=1
-p=0
-initial_num= 1225
+            bottom_range+=1 # if the value tested isn't correct, it adds one and tries again.
+
+initial_num= 1 
 num= initial_num
-numbers=[36,1225]
-square_places=[6,35]
-triangle_places=[8,49]
-# print leap_frog(36)
-for i in range(0,100):
-    ul_num=numbers[len(numbers)-1]
-    penult_num= numbers[len(numbers)-2]
-    quot= (sqrt(ul_num)*1.0)/(sqrt(penult_num)*1.0)
-    #print quot
-    frogged=leap_frog(num, quot)
+numbers=[1]
+square_places=[1]
+triangle_places=[1]
+
+for i in range(0,number_wanted):
+    
+    frogged=leap_frog(num)
     print frogged
     numbers.append(frogged)
-    square_places.append((sqrt(frogged)))
-    triangle_place= (long(sqrt(frogged*8 +1)-1)/2)
-    triangle_places.append(long(triangle_place))
+    square_places.append(int(sqrt(frogged)))
+    triangle_place= (int(sqrt(frogged*8 +1)-1)/2)
+    triangle_places.append(int(triangle_place))
     num=frogged
     
     
@@ -51,13 +45,12 @@ print "Square Places"
 print square_places
 print"Triangle Places"
 print triangle_places
-quotients = [((square_places[i+1]*1.0)/(square_places[i]*1.0)) for i in range(len(square_places)-1)]
+#This next block was used to determine what the approached value for bottom_range was
+'''quotients = [((square_places[i+1]*1.0)/(square_places[i]*1.0)) for i in range(len(square_places)-1)] 
 print "square places quotients"
 print quotients
 diffs = [quotients[i]-quotients[i+1] for i in range(len(quotients)-1)]
 print "differences of quotients"
 print diffs
-print(" %s seconds" %(time.time()-start_time))
-#print sqrt(2440345580851981401.0)/sqrt(73804512832419600.0)
-#print sqrt(2440345580851981401.0*8 +1)
-#print sys.maxint
+'''
+print(" This took %s seconds to do" %(time.time()-start_time))
